c368346e846252bad093b2a6cc125ad586de11ea
mprint2002 <147603714+mprint2002@users.noreply.github.com>
2023-10-11 19:23:07 +0000

    Started creating database stuff

--- Application.py
+++ Application.py
@@ -1,6 +1,9 @@
 # Import flask and datetime module for showing date and time
-from flask import Flask
 import datetime
+import sqlite3
+from sqlite3 import Error
+from flask import Flask
+
  
 x = datetime.datetime.now()
  
@@ -20,7 +23,37 @@ def get_time():
         "programming":"python"
         }
  
+#get each message, get all the data
      
 # Running app
+
+
+
+
+def create_connection(db_file):
+    """ create a database connection to a SQLite database """
+    conn = None
+    try:
+        conn = sqlite3.connect(db_file)
+        print(sqlite3.version)
+    except Error as e:
+        print(e)
+    finally:
+        if conn:
+            conn.close()
+@app.route("/get_messages/<clientID>")
+def getMessages(clientID):
+
+    return json()
+
 if __name__ == '__main__':
     app.run(debug=True)
+    create_connection("data.db")
+    createTables()
+
+    "CREATE TABLE User(userID int primary key, name TEXT, username TEXT);"
+    "CREATE TABLE TherapistUser(therapistuserID int primary key references User(userID));"
+    "CREATE TABLE ClientUser(userID int primary key foreign key references User(userID), TherapistUserID foreign key references TherapistUser(TherapistUserID));"
+
+
+
